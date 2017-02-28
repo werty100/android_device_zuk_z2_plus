83c22581cbb931a7988581bbb0cc9f704d843915
DD3Boh <dade.garberi@gmail.com>
2017-02-28 12:37:22 +0100

    Xenon HD

--- xenonhd.mk
+++ xenonhd.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Xenon HD stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_z2_plus
+PRODUCT_NAME := xenonhd_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
